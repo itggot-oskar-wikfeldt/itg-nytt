1325c0f3d3856674a1bb810568798d73b1fd9765
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-05-08 13:11:15 +0200

    dubidam dam dadubidaba dubidam

--- site/js/script.js
+++ site/js/script.js
@@ -1,17 +1,29 @@
-$(window).scroll(function(){
+$(window).scroll(function () {
 
-    var a = 64;
-    var pos = $(window).scrollTop();
-    if(pos > a) {
-        $("#nav-bar").css({
+    if (window.matchMedia("(min-device-width: 1025px)").matches) {
+
+        const a = 64;
+        let pos = $(window).scrollTop();
+        if (pos > a) {
+            $("#nav-bar-wrapper").css({
+                position: 'fixed',
+                transition: 'top 0.2s',
+                top: '0px',
+                height: '32px'
+            });
+        }
+        else {
+            $("#nav-bar-wrapper").css({
+                position: 'static',
+                height: '64px',
+                top: '-32px'
+            });
+        }
+    } else {
+        $("#nav-bar-wrapper").css({
             position: 'fixed',
-            height: '32px'
-        });
-    }
-    else {
-        $("#nav-bar").css({
-            position: 'static',
-            height: '64px'
+            height: '64px',
+            top: '0px'
         });
     }
 });
