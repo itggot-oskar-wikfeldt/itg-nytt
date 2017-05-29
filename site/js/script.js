24c4a16ce7a4ebf8ad4d1638e9cff0420da3c7ef
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-05-29 11:18:50 +0200

    jjj

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
 
-    if (window.matchMedia("(min-device-width: 1025px)").matches) {
+    if (window.matchMedia("(min-width: 1025px)").matches) {
 
         const a = 64;
         let pos = $(window).scrollTop();
@@ -11,12 +11,20 @@ $(window).scroll(function () {
                 top: '0px',
                 height: '32px'
             });
+            $("header").css({
+                'border-radius': '0px',
+                'height': '48px'
+            });
         }
         else {
             $("#nav-bar-wrapper").css({
                 position: 'static',
                 height: '64px',
                 top: '-32px'
+            });
+            $("header").css({
+                "border-radius": "0",
+                'height': '64px'
             });
         }
     } else {
@@ -27,3 +35,23 @@ $(window).scroll(function () {
         });
     }
 });
+
+function openMenu() {
+    document.getElementById("hamburger-menu").classList.add("open");
+    document.getElementById("scrim").classList.add("open");
+}
+
+function toggleNews() {
+    document.getElementsByClassName("dropdown-menu-wrapper")[0].classList.toggle("open");
+    document.getElementsByClassName("dropdown")[0].classList.toggle("open");
+    document.getElementById("expand-news").classList.toggle("open");
+}
+
+function closeMenu() {
+    document.getElementById("hamburger-menu").classList.remove("open");
+    document.getElementById("scrim").classList.remove("open");
+}
+
+function search() {
+    document.getElementById("input-wrapper").classList.toggle("open");
+}
